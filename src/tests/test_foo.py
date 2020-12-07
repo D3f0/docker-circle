f417fc8fa1d267d12373c1bc18ebcbb9b523d8e0
Nahuel Defossé <nahuel.defosse@gmail.com>
2020-12-07 21:41:02 +0300

    Debub ownership from inside one test

--- src/tests/test_foo.py
+++ src/tests/test_foo.py
@@ -1,6 +1,12 @@
+from pathlib import Path
+from subprocess import check_output
 def test_version():
     assert True
 
 
 def test_bar():
     assert False, "This test is not passing"
+
+
+def test_ownership():
+    check_output('ls -l /test-results')
